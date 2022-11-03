79e1c99841087e8170fdc54e39a2023606f3ad9e
LilitKhalatyan <lkhalatyan@gmail.com>
2022-11-04 00:02:00 +0400

    rebase

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('login').addEventListener('click', () => {
+  document.getElementById('pop').style.display = 'flex';
+});
+
+document.getElementById('pop').style.display = 'none';
+
+document.getElementById('btn-close').addEventListener('click', () => {
+  document.getElementById('pop').style.display = 'none';
+});
