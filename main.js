6b5b8e11bfd6a4c1401b0093fb49d8deab41b985
LilitKhalatyan <lkhalatyan@gmail.com>
2022-11-04 01:22:35 +0400

    add login page

--- main.js
+++ main.js
@@ -7,3 +7,5 @@ document.getElementById('pop').style.display = 'none';
 document.getElementById('btn-close').addEventListener('click', () => {
   document.getElementById('pop').style.display = 'none';
 });
+
+// =============== check if username and password mached ============
