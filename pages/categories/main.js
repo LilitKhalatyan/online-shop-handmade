a55a9ea8dbeb024088c2d407905001ed739afa2b
LilitKhalatyan <lkhalatyan@gmail.com>
2022-11-04 19:27:17 +0400

    restructuring

--- pages/categories/main.js
+++ pages/categories/main.js
@@ -2,7 +2,7 @@ document.getElementById('login').addEventListener('click', () => {
   document.getElementById('pop').style.display = 'flex';
 });
 
-document.getElementById('pop').style.display = 'none';
+// document.getElementById('pop').style.display = 'none';
 
 document.getElementById('btn-close').addEventListener('click', () => {
   document.getElementById('pop').style.display = 'none';
---
+++ pages/main/main.js
@@ -0,0 +1,22 @@
+// =============== managing popup - login and registration ============
+
+const btnLogin = document.getElementById('login');
+const btnSignup = document.getElementById('signup');
+
+btnLogin.addEventListener('click', () => {
+  document.getElementById('pop').style.display = 'flex';
+});
+
+btnSignup.addEventListener('click', (e) => {
+  document.getElementById('popup-body-login').style.display = 'none';
+  document.getElementById('popup-body-signup').style.display = 'flex';
+  e.preventDefault();
+});
+
+document.getElementById('btn-close').addEventListener('click', () => {
+  document.getElementById('pop').style.display = 'none';
+  document.getElementById('popup-body-signup').style.display = 'none';
+  document.getElementById('popup-body-login').style.display = 'flex';
+});
+
+// =============== check if username and password mached ============
