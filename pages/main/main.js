573656d2346498a1a75c891e025563c76897a880
LilitKhalatyan <lkhalatyan@gmail.com>
2022-11-05 19:35:37 +0400

    update structure and add css

--- pages/main/main.js
+++ pages/main/main.js
@@ -15,8 +15,43 @@ btnSignup.addEventListener('click', (e) => {
 
 document.getElementById('btn-close').addEventListener('click', () => {
   document.getElementById('pop').style.display = 'none';
-  document.getElementById('popup-body-signup').style.display = 'none';
-  document.getElementById('popup-body-login').style.display = 'flex';
 });
 
+document.getElementById('btn-close-2').addEventListener('click', () => {
+  document.getElementById('pop').style.display = 'none';
+});
+
+// =============== N A V - 2 ===========
+
+const nav2Items = document.getElementById('home-decor');
+
+nav2Items.addEventListener('click', () => {
+  alert('aa');
+  window.location.href = '../categories/categories.html';
+});
+
+document.getElementsByClassName('nav-2-item').addEventListener('click', () => {
+  window.location.href = '../categories/categories.html';
+});
+
+// =============== swiper ============
+
+let swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 // =============== check if username and password mached ============
